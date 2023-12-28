3067a112820919be192340c178d446afa93f7739
Kai Iemsawat <kaiiemsawat@gmail.com>
2023-12-28 11:29:23 -0500

    404 page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import path from "path";
 
 import rootRouter from "./routes/root.js"; // Import the router using ES module syntax
+import root404 from "./routes/root404.js";
 
 const PORT = process.env.PORT || 3500;
 
@@ -11,5 +12,6 @@ const __dirname = path.dirname(new URL(import.meta.url).pathname);
 const app = express();
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/", rootRouter);
+app.use("*", root404);
 
 app.listen(PORT, () => console.log(`SERVER'S LAUNCHED ON PORT :: ${PORT}`));
